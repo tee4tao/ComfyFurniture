bb188c5a16503f9f8c275cb3e4a00bd8f1b0b06a
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2025-01-07 07:39:45 +0100

    feat: Worked on saved items add to cart button

--- app/(root)/saved-items/page.tsx
+++ app/(root)/saved-items/page.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 import React from "react";
 
 const SavedItems = () => {
-  const { savedItems, removeFromSavedItems } = useCart();
+  const { savedItems, removeFromSavedItems, updateCart } = useCart();
   // todo: change the type of savedItem
   const onDeleteItem = async (savedItem: any) => {
     const loggedIn = await getLoggedInUser();
@@ -58,34 +58,13 @@ const SavedItems = () => {
                 >
                   Remove
                 </button>
-                <Button>Add to Cart</Button>
+                <Button onClick={() => updateCart(savedItem.product, 1)}>
+                  Add to Cart
+                </Button>
               </div>
             </article>
           );
         })}
-        {/* <article className="w-full flex flex-col mb-8 shadow-md p-2 space-y-4">
-          <div className="flex items-center justify-center gap-8 border-b border-gray-300 pb-2">
-            <Image
-              src={"/images/Bedroom.png"}
-              alt=""
-              width={50}
-              height={30}
-              className="object-cover w-20"
-            />
-            <div>
-              <h3 className="text-lg font-bold">Muggo</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Suscipit, dolorum.
-              </p>
-              <p className="font-semibold">$310</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-between p-1 px-2">
-            <button className="text-primary">Remove</button>
-            <Button>Add to Cart</Button>
-          </div>
-        </article> */}
       </div>
     </section>
   );
