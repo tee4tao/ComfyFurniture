dbee5371ba63e46d76fa0c6ce41d26dc3bb570fa
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2025-01-17 10:39:47 +0100

    feat: Enhance error component with image display and refresh button for better user experience

--- app/(root)/error.tsx
+++ app/(root)/error.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 type Props = {
   error: {
     message: string;
@@ -7,7 +10,27 @@ type Props = {
   };
 };
 const error = (error: Props) => {
-  return <div className="mb-8">{error.error.message}</div>;
+  return (
+    <div className="mb-8 flex items-center gap-10">
+      <Image
+        src={"/images/failed.png"}
+        alt="fetch failed"
+        width={100}
+        height={100}
+      />
+      <div className="text-center space-y-4">
+        <div className="">
+          <h3 className="text-xl capitalize font-semibold">
+            {error.error.message}!
+          </h3>
+          <p>Something went wrong</p>
+        </div>
+        <Button className="text-white" onClick={() => window.location.reload()}>
+          Refresh
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default error;
