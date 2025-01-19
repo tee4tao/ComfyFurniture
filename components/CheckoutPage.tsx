3ef489f509d6c43a757d90ae90eebf969f3af154
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2025-01-19 15:58:09 +0100

    feat: Update return URL in CheckoutPage to point to the production site

--- components/CheckoutPage.tsx
+++ components/CheckoutPage.tsx
@@ -55,7 +55,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `http://localhost:3000/payment-success?amount=${amount}`,
+        return_url: `https://comfy-furnitur.vercel.app/payment-success?amount=${amount}`,
       },
     });
 
